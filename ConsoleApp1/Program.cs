aa2b15beb1de1d2c89f4ec3de56d8cb3e44293b8
Zbiku <Zbiku@MSI>
2023-11-18 23:43:38 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿class Zad
+{
+    static void Main(string[] args)
+    {
+        int[] tab = new int[100];
+        Random rand = new Random();
+        for (int i = 0; i < 100; i++)
+        {
+            tab[i] = rand.Next(1, 1000);
+        }
+        for (int i = 0; i < 100; i++)
+        {
+            Console.Write(tab[i] + " ");
+        }
+        Console.ReadKey();
+    }
+}
